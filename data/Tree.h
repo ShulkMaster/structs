71e944a4dd25c6d2e81a7f565120d6cdf21479c8
Yury Rivera <yrivera@applaudostudios.com>
2022-06-04 19:39:39 -0600

    :sparkles: tree insertions

--- data/Tree.h
+++ data/Tree.h
@@ -7,16 +7,19 @@
 namespace Data {
 
     template<typename T>
-    class Tree : public IMutable<TreeNode<T> *> {
+    class Tree : public IMutable<TreeNode<T> *>, public IEnumerable<TreeNode<T> *> {
     private:
         TreeNode<T> *root = nullptr;
         TreeNode<T> *stack = nullptr;
+        int jumps = 0;
+        int count = 0;
 
         bool Insert(TreeNode<T> *node, TreeNode<T> *tree) {
             node->up = tree;
             if (node->value < tree->value) {
                 if (tree->left == nullptr) {
                     tree->left = node;
+                    count++;
                     return true;
                 }
                 return Insert(node, tree->left);
@@ -24,16 +27,38 @@ namespace Data {
 
             if (tree->right == nullptr) {
                 tree->right = node;
+                count++;
                 return true;
             }
             return Insert(node, tree->right);
         }
 
+        TreeNode<T> *JumpToNext(TreeNode<T> *tree) {
+            if (tree->up == nullptr) return tree;
+            auto up = tree->up;
+            for (int i = jumps; i > 0; --i) {
+                jumps--;
+                up = up->up;
+            }
+            if (up->right == nullptr) {
+                while (up->right == nullptr && up != nullptr) {
+                    up = up->up;
+                }
+            }
+            return up;
+        }
+
     public:
+
+        TreeNode<T> * GetRoot() {
+            return root;
+        }
+
         bool Insert(TreeNode<T> *node) override {
             if (root == nullptr) {
                 root = node;
                 stack = root;
+                count++;
                 return true;
             }
             return Insert(node, root);
@@ -45,6 +70,45 @@ namespace Data {
 
         bool Replace(int position) override {
             return false;
+        }
+
+        TreeNode<T> *GetCurrent() override {
+            return stack;
+        }
+
+        bool Next() override {
+            if (stack->left != nullptr) {
+                stack = stack->left;
+                return true;
+            }
+            if (stack->right != nullptr) {
+                stack = stack->right;
+                jumps++;
+                return true;
+            }
+
+            auto next = JumpToNext(stack);
+            if (stack == next || stack == root) {
+                return false;
+            }
+            if(next->right == stack){
+                next = JumpToNext(next);
+            }
+            jumps++;
+            stack = next->right;
+            return true;
+        }
+
+        bool Next(int forward) override {
+            return false;
+        }
+
+        int Count() override {
+            return count;
+        }
+
+        void Reset() override {
+            stack = root;
         }
     };
 
--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
 
 int main() {
     Data::Tree<int> tree;
-    for (int i = 0; i < 10; ++i) {
-        tree.Insert(new Data::TreeNode<int>(i));
-    }
-//    do {
-//        std::cout << tree. << "\n";
-//    } while (graph.Next());
+    tree.Insert(new Data::TreeNode<int>(5));
+    tree.Insert(new Data::TreeNode<int>(2));
+    tree.Insert(new Data::TreeNode<int>(7));
+    tree.Insert(new Data::TreeNode<int>(8));
+    tree.Insert(new Data::TreeNode<int>(9));
+    tree.Insert(new Data::TreeNode<int>(6));
+    tree.Insert(new Data::TreeNode<int>(3));
+    tree.Insert(new Data::TreeNode<int>(1));
+    do {
+        std::cout << tree.GetCurrent()->value << "\n";
+    } while (tree.Next());
     std::cout << std::endl;
     return 0;
 }
